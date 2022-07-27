f1e561b9c727f8ac6c7ad64b1eb07b05fb74e50d
danielphandptp <business.edwardp@gmail.com>
2022-07-26 21:25:00 -0400

    implemented server core functions and connected to DB

---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+import authRoutes from './routes/auth';
+
+const morgan = require('morgan');
+
+// ----------
+// | SERVER |
+// ----------
+// express app
+const app = express();
+
+// database
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => console.log('DB CONNECTION ESTABLISHED'))
+  .catch((err) => console.log('DB CONNECTION ERROR: ', err));
+
+// middlewares
+app.use(express.json({ limit: '4mb' }));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(morgan('dev'));
+
+// route middlewares
+app.use('/api', authRoutes);
+
+// start the server
+app.listen(8000, () => console.log('Server running on port 8000'));
